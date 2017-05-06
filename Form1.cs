6a9406e4a45b09a721a3b3cf1ffd6e1a930de0e6
WLZ@supersight <ifwenvlook@gmail.com>
2017-05-06 10:57:14 +0800

    Version1.0

---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace mysql
+{
+    public partial class Form1 : Form
+    {
+        connect_mysql connect1;
+        MySqlCommand comm;
+        MySqlConnection conn;
+        public Form1()            
+        {            
+            InitializeComponent();
+            connect1 = new connect_mysql { database = "css" };           
+            string connect_str = connect1.connect_str();
+            conn = new MySqlConnection(connect_str);
+            comm = new MySqlCommand(connect1.sqlstr, conn); 
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connect1.ip_mysqlserver = textBox1.Text;
+            connect1.user_id = textBox4.Text;
+            connect1.password = textBox3.Text;
+            if (connect1.connect_sucessfull)
+            { label2.Text = "成功"; MessageBox.Show("连接成功"); }
+            else
+            { label2.Text = "失败"; MessageBox.Show("连接失败"); }            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+            connect1.ip_mysqlserver = textBox1.Text;
+            connect1.user_id = textBox4.Text;
+            connect1.password = textBox3.Text;
+            if (connect1.connect_sucessfull)
+            {
+                label2.Text = "成功";
+                string connect_str = connect1.connect_str();
+                connect1.sqlstr = "SELECT * FROM  record_item ";
+                conn = new MySqlConnection(connect_str);
+                comm = new MySqlCommand(connect1.sqlstr, conn);
+                MySqlDataAdapter adap = new MySqlDataAdapter(comm);
+                DataSet ds = new DataSet();
+                adap.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                textBox2.Text = connect1.sqlstr;
+            }
+
+            else
+            { label2.Text = "失败"; MessageBox.Show("连接失败"); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            connect1.ip_mysqlserver = textBox1.Text;
+            connect1.user_id = textBox4.Text;
+            connect1.password = textBox3.Text;
+            if (connect1.connect_sucessfull)
+            { label2.Text = "成功"; }
+            else
+            { label2.Text = "失败"; }
+            string connect_str = connect1.connect_str();
+            MySqlConnection conn = new MySqlConnection(connect_str);
+            string timemin=dateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm");
+            string timemax=dateTimePicker2.Value.ToString("yyyy-MM-dd HH:mm");
+            string timenow = string.Format("{0:d}", System.DateTime.Now);
+            string table=comboBox1.SelectedItem.ToString();
+            if (table == "record_item" && connect1.connect_sucessfull)
+            {
+                connect1.sqlstr = "DELETE FROM " + table + " WHERE \"" + timemin + "\" < BEGIN_TIME < \"" + timemax + "\"";                
+                comm = new MySqlCommand(connect1.sqlstr, conn);
+                conn.Open();
+                Console.WriteLine(connect1.sqlstr);
+                int iRet = comm.ExecuteNonQuery();//这里返回的是受影响的行数，为int值。可以根据返回的值进行判断是否插入成功。
+                if (iRet > 0)
+                {
+
+                    MessageBox.Show("删除成功");
+
+                }
+                else
+                {
+
+                    MessageBox.Show("该时间段内没有数据");
+                    connect1.sqlstr = "SELECT * FROM " + table + " WHERE \"" + timemin + "\" < BEGIN_TIME < \"" + timemax + "\"";
+                    comm = new MySqlCommand(connect1.sqlstr, conn);
+                    MySqlDataAdapter adap = new MySqlDataAdapter(comm);
+                    DataSet ds = new DataSet();
+                    adap.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+                }
+                conn.Close();//关闭连接
+                textBox2.Text = connect1.sqlstr;
+            }
+            else if (table != "record_item" && connect1.connect_sucessfull)
+            {
+                connect1.sqlstr = "DELETE FROM  " + table;
+                comm = new MySqlCommand(connect1.sqlstr, conn);
+                conn.Open();
+                int iRet = comm.ExecuteNonQuery();//这里返回的是受影响的行数，为int值。可以根据返回的值进行判断是否插入成功。
+                if (iRet > 0)
+                {
+                    Console.WriteLine("this is table: " + table);
+                    Console.WriteLine("清空了 " + table + "中的数据");
+                    MessageBox.Show("删除表成功");
+
+                }
+                else
+                {
+                    connect1.sqlstr = "SELECT *  FROM " + table;
+                    comm = new MySqlCommand(connect1.sqlstr, conn);
+                    MySqlDataAdapter adap = new MySqlDataAdapter(comm);
+                    DataSet ds = new DataSet();
+                    adap.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                    Console.WriteLine("this is table: " + table);
+                    Console.WriteLine(table + "中的数据为空");
+                    MessageBox.Show("删除失败," + table + " 表是空的");
+
+                }
+                conn.Close();//关闭连接
+                textBox2.Text = connect1.sqlstr;
+            }
+            else { MessageBox.Show("无法连接数据库服务"); }  
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = comboBox1.Items.IndexOf("record_item");
+        }
+        
+    }
+}
---
+++ connect_mysql.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace mysql
+{
+    class connect_mysql
+    {        
+        public string ip_mysqlserver;
+        public string user_id;
+        public string password;
+        public string database;
+        public string sqlstr;
+        public MySqlCommand comm;
+
+        public string connect_str()
+        {
+            string x = "server=" + ip_mysqlserver + ";user id=" +
+                user_id + ";password=" + password + ";database=" + database;
+            return x;
+        }
+
+        private bool Connect_sucessfull()
+        {
+            //string mysql_connect_str = "server=127.0.0.1;user id=root;password=;database=css"; //根据自己的设置
+           
+            string mysql_connect_str = "server=" + ip_mysqlserver + ";user id=" +
+                user_id + ";password=" + password + ";database=" + database;
+            MySqlConnection conn = new MySqlConnection(mysql_connect_str);
+            try
+            {
+                conn.Open();
+                Console.WriteLine("连接成功！");
+                conn.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("连接失败！");
+                return false; 
+            }
+            finally
+            {
+                conn.Close();
+                Console.WriteLine("连接关闭！");
+            }
+            
+        }
+        //此时外部只能调用public方法，且调用时不加()
+        public bool connect_sucessfull{ get {return Connect_sucessfull();}}    
+    }
+}
